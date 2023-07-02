80f997cfcf2ed301d4a9d0ca219c319d7b4bbff6
fran-carbo <carbonelli.francisco@gmail.com>
2023-07-01 23:11:53 -0300

    Updated name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <footer class="bg-white dark:bg-gray-900">
             <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
                 <a href="/" className="px-2  inline-flex items-center text-3xl font-semibold tracking-tighter text-black ml-10 md:ml-0 z-20">
-                Birthday Reminder
+                Birthday Assistance
                 </a>
 
                 <p class="text-sm text-gray-600 dark:text-gray-300">© Copyright 2023. All Rights Reserved.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
         <div className="relative flex flex-col w-full p-3 mx-auto bg-[#f1f1f1] md:items-center md:justify-between md:flex-row md:px-6 lg:px-8 z-50">
           <div className="flex flex-row items-center justify-between lg:justify-start">
             <a className="px-2 py-2 inline-flex items-center text-3xl font-semibold tracking-tighter text-black ml-10 md:ml-0 z-20" href="/">
-              Birthday Reminder
+              Birthday Assistance
             </a>
             <button onClick={toggleMenu} className="inline-flex items-center justify-center p-2 text-gray-400 hover:text-black focus:outline-none focus:text-black md:hidden z-50">
               <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
