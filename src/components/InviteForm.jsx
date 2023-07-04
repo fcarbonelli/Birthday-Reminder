fc50104d81e61e32d4da8c8bf578817308caeeed
fran-carbo <carbonelli.francisco@gmail.com>
2023-07-04 16:01:14 -0300

    minor fixes

--- src/components/InviteForm.jsx
+++ src/components/InviteForm.jsx
@@ -97,12 +97,12 @@ export default function InviteForm() {
             {!isOpen && (<ThankYou/>)}
         {isOpen && (
         <div
-          className="fixed top-0 left-0 right-0 z-0 flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0"
+          className=" z-0 flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:p-0"
           aria-labelledby="modal-title"
           role="dialog"
           aria-modal="true"
         >
-          <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:p-0">
             <span
               className="hidden sm:inline-block sm:align-middle sm:h-screen"
               aria-hidden="true"
